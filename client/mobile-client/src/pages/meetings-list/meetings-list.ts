90ce9a15a979fc608c60c8c32fe9224fbe678d9c
Jedrzej Maczan <jedrzej.maczan@capgemini.com>
2017-11-13 21:08:45 +0100

    Meeting list downloading and displaying in progress

--- client/mobile-client/src/pages/meetings-list/meetings-list.ts
+++ client/mobile-client/src/pages/meetings-list/meetings-list.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import {Meeting} from "../../models/meeting";
+import {MeetingService} from "../../providers/meeting-service";
 
 /**
  * Generated class for the MeetingsListPage page.
@@ -19,11 +20,51 @@ export class MeetingsListPage {
   private justStarted: boolean = true;
   private meetingsLoaded: boolean = false;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private meetingService: MeetingService) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad MeetingsListPage');
+    this.fetchMeetingsList().then(
+
+    );
+  }
+
+  ionViewDidEnter() {
+    if (this.justStarted) {
+      this.justStarted = false;
+      this.fetchMeetingsList().then(
+        res => {
+        },
+        err => {
+          console.log("Failed to fetch teams.");
+        }
+      ).then(
+        res => {
+
+        }, err => {
+        }
+      );
+    }
+  }
+
+  fetchMeetingsList() {
+    return new Promise((resolve, reject) => {
+      this.meetingService.getAllMeetings().subscribe(
+        res => {
+          this.meetings = res;
+          this.meetingsLoaded = true;
+        }, err => {
+          this.meetings = [];
+          this.meetingsLoaded = true;
+        }
+      );
+    })
+  }
+
+  doRefresh(refresher) {
+    console.log('TeamsListPage: teams list refresh requested', refresher);
+    this.fetchMeetingsList().then(refresher.complete());
   }
 
 }
